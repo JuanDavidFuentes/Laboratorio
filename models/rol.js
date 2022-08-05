9835e74dd1c40658f840eadcb34ecbde0c8d50c0
APRENDIZ <APRENDIZ@SGLDFPCATSD249.FormacionSena.RED>
2022-08-04 22:25:48 -0500

    cddds

--- models/rol.js
+++ models/rol.js
@@ -3,4 +3,4 @@ import mongoose from "mongoose";
 const RolSchema= new mongoose.Schema({
     rol:{type:String,required:true}
 })
-export default mongoose.model("Rol",RolSchema) 
+export default mongoose.model("Rol",RolSchema)  
