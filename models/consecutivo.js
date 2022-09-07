b891d67a05f36bc26c46bac9387cc316a9ce7a37
unknown <SENA@SGLDFPCATSD249.FormacionSena.RED>
2022-09-06 20:37:10 -0500

    c mamo2

--- models/consecutivo.js
+++ models/consecutivo.js
@@ -4,6 +4,7 @@ const ConsecutivoSchema= new mongoose.Schema({
     numero_cotizacion:{type:Number,default:1},
     informe_No:{type:Number,default:1},
     codMuestra:{type:Number,default:1},
+    iva:{type:Number}
 })
 
 export default mongoose.model("Consecutivo",ConsecutivoSchema)
