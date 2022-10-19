7aa9755fc07adaf98d2bd5aa17770a03df273718
unknown <SENA@SGLDFPCATSD249.FormacionSena.RED>
2022-10-18 22:46:53 -0500

    asdasasdsadasdasdasd

--- models/log.js
+++ models/log.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
-const logSchema= new mongoose.Schema({
-    idUsuario:{type: String},
-    idPut:{type: String},
-    idPost:{type: String},
-    navegador:{type: String},
-    ip:{type: String},
-    fecha: {type: Date,default: Date.now}
+const logSchema = new mongoose.Schema({
+    idUsuario: { type: mongoose.Schema.ObjectId, ref: "Usuario" },
+    idPut: { type: String },
+    idPost: { type: String },
+    navegador: { type: String },
+    ip: { type: String },
+    fecha: { type: Date, default: Date.now }
 })
 
-export default mongoose.model("log",logSchema)
+export default mongoose.model("log", logSchema)
